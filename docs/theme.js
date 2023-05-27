55fe28f70868874835b2f95b5c3c4124309a9a4c
Yan <yannpassaro@gmail.com>
2023-05-26 22:57:03 -0300

    feat: code improvements and refactoring

--- docs/theme.js
+++
@@ -1,7 +0,0 @@
-function toggleTheme() {
-    const body = document.querySelector('body');
-    const sidebar = document.querySelector('.sidebar');
-
-    body.classList.toggle('dark-theme');
-    sidebar.classList.toggle('dark-theme');
-}
